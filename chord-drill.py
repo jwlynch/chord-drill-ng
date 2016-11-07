767112e9d43bbf9a179a034f167e49a5ec2e4a5f
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-07 05:39:04 -0800

    add basic 7th chords

--- chord-drill.py
+++ chord-drill.py
@@ -23,8 +23,36 @@ sus_triads = []
 for i in maj_roots:
     sus_triads.append(i + " sus")
 
+maj7s = []
+for i in maj_roots:
+    maj7s.append(i + " maj 7")
+
+maj7suss = []
+for i in maj_roots:
+    maj7suss.append(i + " maj 7 sus")
+
+dom7s = []
+for i in maj_roots:
+    dom7s.append(i + "7")
+
+dom7suss = []
+for i in maj_roots:
+    dom7suss.append(i + "7sus")
+
+min7s = []
+for i in maj_roots:
+    min7s.append(i + " min 7")
+
+min7b5s = []
+for i in maj_roots:
+    min7b5s.append(i + " min 7b5")
+
+dim7s = []
+for i in maj_roots:
+    dim7s.append(i + " dim 7")
+
 # set this to a list of whatever you want to drill on
-drill_seq = sus_triads.copy()
+drill_seq = dim7s.copy()
 
 while drill_seq:
     nxt = choice(drill_seq)
