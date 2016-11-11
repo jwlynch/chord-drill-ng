e7b11c7db4c4350c5f9200d54c2e9b8a7694794d
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-11 00:48:52 -0800

    use '[:]' on lists to signify we want to operate on a copy of the list rather than the list itself

--- chord-drill.py
+++ chord-drill.py
@@ -9,28 +9,28 @@ aug_roots = ['C','F','Bb','Eb','Ab','Db','Gb','Cb','E','A','D','G']
 
 triad_list = []
 
-maj_triads = maj_roots.copy()
-triad_list += maj_triads
+maj_triads = maj_roots[:]
+triad_list += maj_triads[:]
 
 min_triads = []
 for i in maj_roots:
     min_triads.append(i + " m")
-triad_list += min_triads
+triad_list += min_triads[:]
 
 dim_triads = []
 for i in maj_roots:
     dim_triads.append(i + " dim")
-triad_list += dim_triads
+triad_list += dim_triads[:]
 
 aug_triads = []
 for i in aug_roots:
     aug_triads.append(i + " aug")
-triad_list += aug_triads
+triad_list += aug_triads[:]
 
 sus_triads = []
 for i in maj_roots:
     sus_triads.append(i + " sus")
-triad_list += sus_triads
+triad_list += sus_triads[:]
 
 # Seventh chords
 
@@ -39,37 +39,37 @@ seventh_list = []
 maj7s = []
 for i in maj_roots:
     maj7s.append(i + " maj 7")
-seventh_list += maj7s
+seventh_list += maj7s[:]
 
 maj7suss = []
 for i in maj_roots:
     maj7suss.append(i + " maj 7 sus")
-seventh_list += maj7suss
+seventh_list += maj7suss[:]
 
 dom7s = []
 for i in maj_roots:
     dom7s.append(i + "7")
-seventh_list += dom7s
+seventh_list += dom7s[:]
 
 dom7suss = []
 for i in maj_roots:
     dom7suss.append(i + "7sus")
-seventh_list += dom7suss
+seventh_list += dom7suss[:]
 
 min7s = []
 for i in maj_roots:
     min7s.append(i + " min 7")
-seventh_list += min7s
+seventh_list += min7s[:]
 
 min7b5s = []
 for i in maj_roots:
     min7b5s.append(i + " min 7b5")
-seventh_list += min7b5s
+seventh_list += min7b5s[:]
 
 dim7s = []
 for i in maj_roots:
     dim7s.append(i + " dim 7")
-seventh_list += dim7s
+seventh_list += dim7s[:]
 
 whole_list = triad_list + seventh_list
 
