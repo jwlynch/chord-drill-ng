061763cf3d13d2689e57f58911c13e9ea8a092ea
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-30 21:36:18 -0800

    chord-drill.py: add imports of chord module, add local copies of 'global' vars

--- chord-drill.py
+++ chord-drill.py
@@ -2,9 +2,14 @@
 
 from random import choice
 
+from chords import triads
+from chords import sevenths
+from chords import common
 
-# Seventh chords
+triad_list = triads.triad_list
+seventh_list = sevenths.seventh_list
 
+big_four_list = common.big_four_list
 
 whole_list = triad_list + seventh_list
 
