05bd20f19ed59eec181d2f834677cf9052dada79
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-30 20:58:14 -0800

    factor the chords from chord-drill.py into chords module, chords/triads.py, chords/common.py

--- chord-drill.py
+++ chord-drill.py
@@ -2,40 +2,6 @@
 
 from random import choice
 
-maj_roots = ['C','F','Bb','Eb','Ab','Db','F#','B','E','A','D','G']
-aug_roots = ['C','F','Bb','Eb','Ab','Db','Gb','Cb','E','A','D','G']
-
-# chords for 95 - 98% of pop songs
-big_four_list = [] 
-
-# Triads
-
-triad_list = []
-
-maj_triads = maj_roots[:]
-triad_list += maj_triads[:]
-big_four_list += maj_triads[:]
-
-min_triads = []
-for i in maj_roots:
-    min_triads.append(i + "m")
-triad_list += min_triads[:]
-big_four_list += min_triads[:]
-
-dim_triads = []
-for i in maj_roots:
-    dim_triads.append(i + " dim")
-triad_list += dim_triads[:]
-
-aug_triads = []
-for i in aug_roots:
-    aug_triads.append(i + " aug")
-triad_list += aug_triads[:]
-
-sus_triads = []
-for i in maj_roots:
-    sus_triads.append(i + " sus")
-triad_list += sus_triads[:]
 
 # Seventh chords
 
