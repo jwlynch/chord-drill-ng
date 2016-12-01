f898522ab3ec90062d347ce7d67aae71e9cb7d05
Jim Lynch <jim@jam.sessionsnet.org>
2016-11-30 18:15:12 -0800

    adjust chord names, compact them

--- chord-drill.py
+++ chord-drill.py
@@ -18,7 +18,7 @@ big_four_list += maj_triads[:]
 
 min_triads = []
 for i in maj_roots:
-    min_triads.append(i + " m")
+    min_triads.append(i + "m")
 triad_list += min_triads[:]
 big_four_list += min_triads[:]
 
@@ -43,12 +43,12 @@ seventh_list = []
 
 maj7s = []
 for i in maj_roots:
-    maj7s.append(i + " maj 7")
+    maj7s.append(i + "maj7")
 seventh_list += maj7s[:]
 
 maj7suss = []
 for i in maj_roots:
-    maj7suss.append(i + " maj 7 sus")
+    maj7suss.append(i + "maj7sus")
     seventh_list += maj7suss[:]
 
 dom7s = []
@@ -64,18 +64,18 @@ seventh_list += dom7suss[:]
 
 min7s = []
 for i in maj_roots:
-    min7s.append(i + " min 7")
+    min7s.append(i + "m7")
 seventh_list += min7s[:]
 big_four_list += min7s[:]
 
 min7b5s = []
 for i in maj_roots:
-    min7b5s.append(i + " min 7b5")
+    min7b5s.append(i + "m7b5")
 seventh_list += min7b5s[:]
 
 dim7s = []
 for i in maj_roots:
-    dim7s.append(i + " dim 7")
+    dim7s.append(i + "dim7")
 seventh_list += dim7s[:]
 
 whole_list = triad_list + seventh_list
