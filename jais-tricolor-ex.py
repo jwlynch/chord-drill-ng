4902fea36914a5887f449a4a9e2453ae8231737f
Jim Lynch <jim@jam.sessionsnet.org>
2017-02-03 03:54:10 -0800

    add chord qualities (described as green stack)

--- jais-tricolor-ex.py
+++ jais-tricolor-ex.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
 roots = ["A", "Bb", "B", "C", "C#/Db", "D", "Eb", "E", "F", "F#", "G", "Ab"]
+majtriad = 0
+mintriad = 1
+dom7th = 2
+min7th = 3
+cho_qu = [majtriad, mintriad, dom7th, min7th]
+qualities = cho_qu * 3
