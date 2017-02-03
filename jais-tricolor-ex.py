dbdfe0ea500d1b59b43902b7316c37cb51bf40b3
Jim Lynch <jim@jam.sessionsnet.org>
2017-02-03 07:24:16 -0800

    first try at chose_chord()

--- jais-tricolor-ex.py
+++ jais-tricolor-ex.py
@@ -37,3 +37,41 @@ def init():
     cq = qualities[:]
     cv = top_voices[:]
 
+# choose a chord, return a string describing it
+
+def choose_chord():
+    if len(cr) <= 0:
+        print("choice pool is empty")
+        sys.exit(0)
+
+    # pick a root, quality and voicing
+    root = choice(cr)
+    qual = choice(cq)
+    voic = choice(cv)
+
+    qual_str = ""
+    if qual == majtriad:
+        pass
+    elif qual == mintriad:
+        qual_str = "m"
+    elif qual == dom7th:
+        qual_str = "7"
+    elif qual == min7th:
+        qual_str = "m7"
+
+    voicing_str = ""
+    if qual in [dom7th, min7th]:
+        if voic == v_r:
+            voicing_str = "root on top"
+        elif voic == v_3:
+            voicing_str = "third on top"
+        elif voic == v_5:
+            voicing_str = "fifth on top"
+        elif voic in [v_7_r, v_7_3, v_7_5]:
+            voicing_str = "seventh on top"
+    #elif qual in [majtriad, mintriad]:
+
+    print("root %s, qual %s, voicing %s" % (root, qual_str, voicing_str))
+
+    return "%s%s %s" % (root, qual_str, voicing_str)
+
