d4992904a149695803e1e3c553502797c5904c1e
Jim Lynch <jim@jam.sessionsnet.org>
2017-02-03 04:07:16 -0800

    add chord voicing (spec'd by top note, and described as blue stack)

--- jais-tricolor-ex.py
+++ jais-tricolor-ex.py
@@ -12,3 +12,11 @@ cho_qu = [majtriad, mintriad, dom7th, min7th]
 qualities = cho_qu * 3
 
 # chord voicing
+v_r = 0
+v_3 = 1
+v_5 = 2
+v_7_r = 3
+v_7_3 = 4
+v_7_5 = 5
+tr_topvoice = [v_r, v_3, v_5]
+top_voices = tr_topvoice + [v_7_r] + tr_topvoice + [v_7_3] + tr_topvoice + [v_7_5]
