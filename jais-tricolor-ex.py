ad8aeb2e527dd63567218b5da3da07c61f5751c1
Jim Lynch <jim@jam.sessionsnet.org>
2017-02-03 04:05:13 -0800

    add comments and some blank lines for readability

--- jais-tricolor-ex.py
+++ jais-tricolor-ex.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
+# chord roots
 roots = ["A", "Bb", "B", "C", "C#/Db", "D", "Eb", "E", "F", "F#", "G", "Ab"]
+
+# chord qualities
 majtriad = 0
 mintriad = 1
 dom7th = 2
 min7th = 3
 cho_qu = [majtriad, mintriad, dom7th, min7th]
 qualities = cho_qu * 3
+
+# chord voicing
