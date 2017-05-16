7815cdde8fe3ef752b6fe15274b0e19306fb5b80
Jim Lynch <jim@jam.sessionsnet.org>
2017-05-16 00:36:07 -0700

    just repeat do_12() over and over

--- jais-tricolor-ex.py
+++ jais-tricolor-ex.py
@@ -174,21 +174,4 @@ def input_y_or_n(inp_str):
 
 
 while True:
-    tune_p = True
-
-    input("hit enter when ready to start new session:")
-    print("STARTING NEW SESSION")
-
-    while tune_p:
-        tune_p = input_y_or_n("do you have a tune to work on? ")
-
-        if tune_p:
-            do_12()
-            do_12()
-
-            print("NOW WORK ON TUNE")
-            input("hit enter when done working on the tune:")
-
     do_12()
-    do_12()
-    print("DONE FOR THIS SESSION")
