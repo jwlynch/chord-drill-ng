071111a43be527511471921beb355abc6c3cf0b0
Jim Lynch <jim@jam.sessionsnet.org>
2018-02-14 12:45:43 -0800

    add function mk_maj_key() to help create the quizzing database

--- diatonic-quiz.py
+++ diatonic-quiz.py
@@ -5,3 +5,13 @@ from chords import sevenths
 
 from chords.common import voicing_objects
 
+def mk_maj_key(root_list):
+    qual_list = ["maj7","m7","m7","maj7","7","m7","m7b5"]
+    qual_obj_list = []
+    for qual in qual_list:
+        qual_obj_list.append(voicing_objects[qual])
+
+    result = list(zip(root_list, qual_obj_list))
+
+    return result
+
