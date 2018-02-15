2a977bce8476a7b936236c56a71e5997a18a381a
Jim Lynch <jim@jam.sessionsnet.org>
2018-02-15 01:38:33 -0800

    add function names and corresponding quality strings

--- diatonic-quiz.py
+++ diatonic-quiz.py
@@ -32,3 +32,6 @@ keys = {"C":keyC, "Db":keyDb, "D":keyD, "Eb":keyEb, "E":keyE, "F":keyF, "F#":key
 
 # in keys.keys, the first keys is my var, the second is Dict.keys()
 key_strings = list(keys.keys())
+
+functions = ["Imaj7","IIm7","IIIm7","IVmaj7","V7","VIm7","VIIm7b5"]
+qualities = ["maj7","m7","m7","maj7","7","m7","m7b5"]
